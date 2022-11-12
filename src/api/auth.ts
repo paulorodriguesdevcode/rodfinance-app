e7f1d432339f41fd3848b6b880ccb1665119a12f
Paulo Rodrigues <paulo-roberto-caetano-rodrigues@impulso.network>
2022-11-12 09:11:14 -0300

    feat: auth method and axios instance

---
+++ src/api/auth.ts
@@ -0,0 +1,14 @@
+
+import { api } from "./api";
+import Cookies from "js-cookie";
+
+export async function login(dto: {
+    email: string | undefined;
+    password: string | undefined;
+}) {
+    if (dto.email && dto.password) {
+        const loginResp = await api.post("/auth", dto);
+        Cookies.set("access_token", loginResp.data.access_token);
+        return loginResp;
+    }
+}
