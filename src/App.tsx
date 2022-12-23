4442db7d61ca42abcd4aef0aad5933f7a2fa62bf
Paulo Rodrigues <P39210@parceirosedu.com.br>
2022-12-23 18:08:45 -0300

    fix: name files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./Pages/Home";
-import { Login } from "./Pages/Login";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
 
 function App() {
   return (
